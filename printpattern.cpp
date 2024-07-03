ea42e77c8d177daff3da532a46bc1da4323612d3
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-03 14:30:54 +0200

    added instruction for printpattern

--- printpattern.cpp
+++ printpattern.cpp
@@ -1,3 +1,27 @@
+// You are given a number n. You need to print the pattern for the given value of n.
+
+// For n = 2 the pattern will be 
+// 2 2 1 1
+// 2 1
+
+// For n = 3 the pattern will be 
+// 3 3 3 2 2 2 1 1 1
+// 3 3 2 2 1 1
+// 3 2 1
+
+// Note: Instead of printing a new line print a "$" without quotes. After printing the total output, end of the line("$") is expected.
+
+// Examples :
+
+// Input: 2
+// Output: 2 2 1 1 $2 1 $
+
+// Input: 3
+// Output: 3 3 3 2 2 2 1 1 1 $3 3 2 2 1 1 $3 2 1 $
+
+
+// Constraints:
+// 1 <= n <= 40
 #include <iostream>
 
 void print_pattern(int n){
@@ -11,5 +35,5 @@ void print_pattern(int n){
     }
 }
 int main(){
-    print_pattern(3);
+    print_pattern(40);
 }
