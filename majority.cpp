42c99d14a17c9cff085030a84ce1d7edc67ccb07
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-06 17:39:16 +0200

    added majority desc

--- majority.cpp
+++ majority.cpp
@@ -1,3 +1,22 @@
+// Problem Description
+ 
+ 
+// Given an array of size N, find the majority element. The majority element is the element that appears more than floor(N/2) times.
+
+// You may assume that the array is non-empty and the majority element always exist in the array.
+
+
+// Problem Constraints
+// 1 <= |A| <= 106
+// 1 <= Ai <= 109
+
+
+// Input Format
+// The first argument is an integer array A.
+
+
+// Output Format
+// Return the majority element.
  #include<math.h>
 #include <iostream>
  int maxOfArray(const int* A, int size)
