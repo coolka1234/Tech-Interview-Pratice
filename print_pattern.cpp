865c10a282365aabe808f49817796bb83b18321d
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-08 13:55:28 +0200

    work on recursive pattern

---
+++ print_pattern.cpp
@@ -0,0 +1,35 @@
+// Print a sequence of numbers starting with nn, without using a loop. Replace nn with n−5n - 5n−5 until n≤0n \leq 0n≤0. Then, replace n with n+5n + 5n+5 until nn regains its initial value. Complete the function pattern(n) which takes n as input and returns a list containing the pattern.
+
+// Examples
+
+// Input: n = 16
+// Output: 16 11 6 1 -4 1 6 11 16
+// Explanation: The value decreases until it is greater than 0. After that it increases and stops when it becomes 16 again.
+
+// Input: n = 10
+// Output: 10 5 0 5 10
+// Explanation: It follows the same logic as per the above example.
+
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(n)
+
+// Constraints:
+// -105 ≤ n ≤ 105
+#include <iostream>
+void print_pattern(int number, int orginal_number, bool first_iter){
+    if (number==orginal_number && !first_iter){
+        std::cout<<number<<" ";
+        return;
+    }
+    std::cout<<number<<" ";
+    if (number>0){
+        print_pattern(number-5, orginal_number=orginal_number, first_iter=false);
+    }
+    else{
+      print_pattern(number+5, orginal_number=orginal_number, first_iter=false);  
+    }
+}
+int main(){
+    print_pattern(16, 16, true);
+    return 0;
+}
