9129f74c59fe3f0efce39aca9587174bbc529264
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-08-03 12:06:00 +0200

    multiplying two string problem

---
+++ c++/multiply_string.cpp
@@ -0,0 +1,36 @@
+// Given two numbers as strings s1 and s2. Calculate their Product.
+
+// Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers. You don't need to specify '+' sign in the begining of positive numbers.
+
+// Example 1:
+
+// Input:
+// s1 = "0033"
+// s2 = "2"
+// Output:
+// 66
+
+// Example 2:
+
+// Input:
+// s1 = "11"
+// s2 = "23"
+// Output:
+// 253
+
+// Your Task: You don't need to read input or print anything. Your task is to complete the function multiplyStrings() which takes two strings s1 and s2 as input and returns their product as a string.
+#include <iostream>
+using namespace std;
+int multiply_string(string s1, string s2){
+    char signs[2]={s1[0],s2[0]};
+    bool negative_product=false;
+    if(signs[0]=='-' || signs[1]=='-'){
+        negative_product = (signs[0]!=signs[1]);
+    }
+    int l1=s1.size();
+    int l2=s2.size();       
+}
+
+int main(){
+    return 0;
+}
