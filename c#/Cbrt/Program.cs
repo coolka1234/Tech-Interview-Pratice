9745f506fa04d1a9f3002b8a4d87286506136e83
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-11-22 16:47:50 +0100

    done and working

--- c#/Cbrt/Program.cs
+++ c#/Cbrt/Program.cs
@@ -3,7 +3,22 @@ using System;
 namespace Program{
     class Program{
         static void Main(string[] args){
+                printCbrt(1000);
+            }
+        public static void printCbrt(int N){
+            for (int i = 1; i <= N; i++){
+                for (int j = 0; j <= N; j++){
+                    if (i * i * i + j * j * j == N){
+                        Console.WriteLine("Cbrt of " + N + " is " + i + " and " + j);
+                        return;
+                    }
+
+                }
+            }
+
         }
-    }
+   }
+
+
 }
 
--- c#/Cbrt/obj/Debug/net8.0/Cbrt.AssemblyInfo.cs
+++ c#/Cbrt/obj/Debug/net8.0/Cbrt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd4a8a86cd4305b7fd5d90d941efbfcef3b2bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e119c70cbc34f407c37b6615be7b8eddcad9c298")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
