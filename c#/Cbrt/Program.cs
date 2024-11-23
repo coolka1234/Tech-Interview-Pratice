f8ed891fa6bb946c3bed98975c2ed38b07bfc128
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-11-23 20:02:29 +0100

    tested soultion

--- c#/Cbrt/Program.cs
+++ c#/Cbrt/Program.cs
@@ -4,6 +4,11 @@ namespace Program{
     class Program{
         static void Main(string[] args){
                 printCbrt(1000);
+                System.Console.WriteLine("Bubble Sort");
+                int[] arr = bubble_sort();
+                for (int i = 0; i < arr.Length; i++){
+                    Console.Write(arr[i] + " ");
+                }
             }
         public static void printCbrt(int N){
             for (int i = 1; i <= N; i++){
@@ -13,9 +18,24 @@ namespace Program{
                         return;
                     }
 
+
                 }
             }
 
+        }
+        public static int[] bubble_sort(){
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++){
+                for (int j = 0; j < n - i - 1; j++){
+                    if (arr[j] > arr[j + 1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
         }
    }
 
--- c#/Cbrt/obj/Debug/net8.0/Cbrt.AssemblyInfo.cs
+++ c#/Cbrt/obj/Debug/net8.0/Cbrt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e119c70cbc34f407c37b6615be7b8eddcad9c298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9745f506fa04d1a9f3002b8a4d87286506136e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cbrt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
