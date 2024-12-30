ceea36adfce0608b5e86e37dbe7fb997b73d2da7
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-12-30 11:27:17 +0100

    fixed the logic

--- c++/binary_tree.cpp
+++ c++/binary_tree.cpp
@@ -2,12 +2,21 @@
 #include <vector>
 
 using namespace std;
+template <typename T>
+class Node{
+private:
+    Node<T> *parent;
+    T current;
+    Node<T> *left;
+    Node<T> *right;
+public:
+    Node<T>();
+    Node<T>(Node<T> *parent, Node<T> *left, Node<T>* right);
+};
 template <typename T> 
 class MyBinaryTree{
-    private T currentNode;
-    private T* parent;
-    private T* lChild;
-    private T* rChild;
+    private:
+    Node<T> root;
     public:
     MyBinaryTree();
     MyBinaryTree(T arr[], int s);
@@ -15,4 +24,19 @@ class MyBinaryTree{
     void add(T elem);
     vector<T> bfs();
 
-};
+ };
+
+ template <typename T>
+ MyBinaryTree<T>::MyBinaryTree(T arr[], int s){
+    if(s<=0){
+        return;
+    }
+    else{
+
+    }    
+ }
+
+int main()
+{
+    return 0;
+}
