461602e66617177b443d0760a70b490cf94c02d3
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-12-30 10:56:42 +0100

    overall classes added

--- c++/binary_tree.cpp
+++ c++/binary_tree.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 template <typename T> 
 class MyBinaryTree{
     private T currentNode;
-    private T parent;
-    private T lChild;
-    private T rChild;
+    private T* parent;
+    private T* lChild;
+    private T* rChild;
+    public:
+    MyBinaryTree();
+    MyBinaryTree(T arr[], int s);
+    MyBinaryTree(vector<T> vec);
+    void add(T elem);
+    vector<T> bfs();
+
 };
