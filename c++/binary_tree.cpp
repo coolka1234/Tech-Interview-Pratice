9282925d329bf88b463e86c8536f745ddcb5b66f
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-01-03 19:11:53 +0100

    debugging now

--- c++/binary_tree.cpp
+++ c++/binary_tree.cpp
@@ -3,54 +3,60 @@
 #include <queue>
 
 using namespace std;
-template <typename T> 
+template <typename K> 
 class MyBinaryTree{
     private:
-    template <typename T>
     class Node{
     protected:
-        Node<T> *parent;
-        T current;
-        Node<T> *left;
-        Node<T> *right;
+        Node *parent;
+        K current;
+        Node *left;
+        Node *right;
     public:
-        Node<T>();
-        Node<T>(Node<T> *parent, Node<T> *left, Node<T>* right);
+        Node();
+        Node(Node *parent, Node *left, Node* right);
         friend class MyBinaryTree;
     };
 
-    Node<T> root;
+    Node root;
     public:
     MyBinaryTree();
-    MyBinaryTree(T arr[], int s);
-    MyBinaryTree(vector<T> vec);
-    void add(T elem);
-    vector<T> bfs();
+    MyBinaryTree(K arr[], int s);
+    MyBinaryTree(vector<K> vec);
+    void add(K elem);
+    vector<K> bfs();
 
  };
+ template <typename K>
+ MyBinaryTree<K>::Node::Node(){
+    current=NULL;
+    this->left=nullptr;
+    this->right=nullptr;
+ }
 
- template <typename T>
- MyBinaryTree<T>::MyBinaryTree(T arr[], int s){
+ template <typename K>
+ MyBinaryTree<K>::MyBinaryTree(K arr[], int s){
     if(s<=0){
         return;
     }
     else{
-        Node<T> currNode=root;
+        MyBinaryTree::root.current=arr[0];
+        MyBinaryTree::Node currNode=root;
         currNode.parent=nullptr;
         currNode.current=arr[0];
         currNode.left=nullptr;
         currNode.right=nullptr;
-        Node<T>* leftMost=nullptr;
+        MyBinaryTree::Node* leftMost=nullptr;
         for (int i=1;i<s;i++){
             if(currNode.left==nullptr){
-                *(currNode.left)->current=arr[i];
+                (currNode.left)->current=arr[i];
                 leftMost=currNode.left;
             }
             else if(currNode.right==nullptr){
-                *(currNode.right)->current=arr[i];
+                (currNode.right)->current=arr[i];
             }
             else if(currNode.parent!=nullptr && currNode.parent->right==nullptr){
-                *(currNode.parent->right)=arr[i];
+                (currNode.parent->right)->current=arr[i];
                 currNode=* (currNode.parent->right);
             }
             else{
@@ -58,17 +64,18 @@ class MyBinaryTree{
             }
         }
     }    
+    cout<<"Constructed tree"<<endl;
  }
-template <typename T>
-vector<T> MyBinaryTree<T>::bfs(){
-    queue<Node<T>> bsfQueue;
-    vector<T> result;
+template <typename K>
+vector<K> MyBinaryTree<K>::bfs(){
+    queue<Node> bsfQueue;
+    vector<K> result;
     if(root.current!=NULL){
         bsfQueue.push(root);
         result.push_back(root.current);
     }
     while(!bsfQueue.empty()){
-        Node<T> n=bsfQueue.front();
+        Node n=bsfQueue.front();
         bsfQueue.pop();
         if(n.left!=nullptr){
         if(find(result.begin(), result.end(), *n.left)!=result.end()){
@@ -92,6 +99,7 @@ vector<T> MyBinaryTree<T>::bfs(){
 int main()
 {
     int arr[9]={1,2,3,4,5,6,7,8,9};
+    cout<<"main"<<endl;
     MyBinaryTree<int> tree=MyBinaryTree<int>(arr, 9);
     return 0;
 }
