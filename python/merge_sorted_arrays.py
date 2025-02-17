364a4a8013e9418700389d39fd254639be3febd1
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-17 12:39:07 +0100

    addeed rem dup 2

--- python/merge_sorted_arrays.py
+++ python/merge_sorted_arrays.py
@@ -16,7 +16,6 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
             j+=1
         k+=1
     print(res)
-    print("completed")
 
 if __name__ == '__main__':
     merge([1,2,3,0,0,0],3,[2,5,6],3)
