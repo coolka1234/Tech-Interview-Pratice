3b62fed67ada8fdbe56669666c2bdc58bb5aa366
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-25 14:24:42 +0100

    finished in o(n)

--- python/two_sum.py
+++ python/two_sum.py
@@ -3,8 +3,17 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 #
 # You can return the answer in any order.
-def find_sum(array, target)->tuple(int, int):
-
+def find_sum(array, target):
+    scores={}
+    i=0
+    for i, num in enumerate(array):
+        complement=target-num
+        if complement in scores:
+            return scores[complement], i
+        else:
+            scores[num]=i
+    return []
+    
 
 if __name__=="__main__":
     array=[3,2,4]
