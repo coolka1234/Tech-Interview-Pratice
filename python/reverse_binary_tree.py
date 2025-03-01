61f2e9740bb4cb5281373642d90a17822a28da23
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-03-01 20:21:05 +0100

    some progress

--- python/reverse_binary_tree.py
+++ python/reverse_binary_tree.py
@@ -1,11 +1,12 @@
 # Given the root of a binary tree, invert the tree, and return its root.
 
-def reverse(root, result):
+def reverse(root, result=[]):
     root_len=len(root)
     if root_len<=2:
-        return [root[1], root[0]]
-    elif root_len>=4:
-
+        return [root[1], root[0] + result]
+    else:
+        result+=reverse(root[2:root_len], [])
+        
 
 root = [4,2,7,1,3,6,9]
 print(reverse(root))
